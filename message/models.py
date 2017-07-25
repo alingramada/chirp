8f3809260c02ebe7d097fc50be0f10197c43731d
alingramada <alin.gramada2012@gmail.com>
2017-07-25 09:45:38 +0300

    project

--- message/models.py
+++ message/models.py
@@ -23,5 +23,10 @@ class Like(TimestampModel):
 
 
 class Follow(TimestampModel):
+    class Meta:
+        unique_together = [("followed_user", "following_user"),]
     followed_user = models.ForeignKey(User, related_name='followed_by')
     following_user = models.ForeignKey(User, related_name='following')
+
+    def __str__(self):
+        return "User {0} follows user {1}".format(self.following_user.username, self.followed_user.username)
